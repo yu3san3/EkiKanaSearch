a867408f58b0d27db3e0dd319d19754ddac23c89
yu3san3 <orange12817@yahoo.co.jp>
2023-05-27 00:21:01 +0900

    Initial Commit

---
+++ KanaSearch/KanaSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanaSearchApp.swift
+//  KanaSearch
+//
+//  Created by 丹羽雄一朗 on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct KanaSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
