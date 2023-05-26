b419b084475b9b5f7335c6df416703bdcc0a9340
yu3san3 <orange12817@yahoo.co.jp>
2023-05-27 02:28:12 +0900

    StationListViewModel.swiftを追加

--- KanaSearch/Model/NearbyStationData.swift
+++ KanaSearch/Model/NearbyStationData.swift
@@ -25,3 +25,9 @@ struct Station: Codable {
     let prev: String?
     let next: String?
 }
+
+let mockStationData: [Station] = [
+    Station(name: "日本へそ公園", prefecture: "兵庫県", line: "JR加古川線", x: 134.997633, y: 35.002069, distance: "320m", prev: Optional("比延"), next: Optional("黒田庄")),
+    Station(name: "比延", prefecture: "兵庫県", line: "JR加古川線", x: 134.995733, y: 34.988773, distance: "1310m", prev: Optional("新西脇"), next: Optional("日本へそ公園")),
+    Station(name: "黒田庄", prefecture: "兵庫県", line: "JR加古川線", x: 134.992522, y: 35.022689, distance: "2620m", prev: Optional("日本へそ公園"), next: Optional("本黒田"))
+]
---
+++ KanaSearch/ViewModel/StationListViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  StationListView.swift
+//  KanaSearch
+//
+//  Created by 丹羽雄一朗 on 2023/05/27.
+//
+
+import Foundation
+
+final class StationListViewModel: ObservableObject {
+    @Published var stationData: [Station] = []
+    
+    @Published var shouldShowLoadingIndicator: Bool = false
+    @Published var shouldShowAlert: Bool = false
+    @Published var error: APIError?
+    
+    private let nearbyStationFetcher = NearbyStationFetcher()
+    
+    func fetchStationData() {
+        Task { @MainActor in
+            shouldShowLoadingIndicator = true
+            defer {
+                shouldShowLoadingIndicator = false
+            }
+            
+            do {
+                stationData = try await nearbyStationFetcher.fetchCityData()
+            } catch {
+                if let apiError = error as? APIError {
+                    self.error = apiError
+                    shouldShowAlert = true
+                } else if let error = error as? URLError, error.code == URLError.notConnectedToInternet {
+                    self.error = APIError.network
+                    shouldShowAlert = true
+                } else {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
